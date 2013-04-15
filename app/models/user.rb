a5dc0a91b01f532ded27bc93c5a6755d1d3442e4
Debjyoti Majumder <debjyoti.majumder@gmail.com>
2013-04-15 21:41:29 +0530

    Profile picture caching. Showing already uploaded picture in edit profile screen.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :image
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :image, :image_cache
   # attr_accessible :title, :body
   validates :name, presence: true
   has_many :questions
